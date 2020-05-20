659a0dd2827edae0896cd8c177cd2816344ba5a0
jutony <anyupatov@gmail.com>
2020-05-20 19:15:44 +0300

    changes to fix errors in k8s pod v3

--- activities_python/pythonutils/mysql_adapter.py
+++ activities_python/pythonutils/mysql_adapter.py
@@ -11,7 +11,7 @@ from requests import ReadTimeout
 from requests import Timeout
 
 from ..constants.basic_constants import BasicConstants
-from .models.template_token import TemplateToken
+# from .models.template_token import TemplateToken
 from .mysql_error import MySQLError
 from .utils import get_proxies
 
